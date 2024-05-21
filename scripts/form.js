87b499306534691332a1a5dc376c098729468cc1
lyipei <lin23027@byui.edu>
2024-05-21 17:58:30 -0600

    form

---
+++ scripts/form.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', () => {
+    const year = document.getElementById('currentyear');
+    year.innerHTML = new Date().getFullYear();
+});
+
+window.addEventListener('load', () => {
+    const modified = document.getElementById('lastModified');
+    modified.innerHTML = document.lastModified;
+});
+const products = [
+    {
+      id: fc-1888,
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: fc-2050,
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: fs-1987,
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: ac-2000,
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: jj-1969,
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+  
+  const productSelect = document.getElementById('productName');
+
+  products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+  
