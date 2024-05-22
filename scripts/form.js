606e07f943d772b39841b47ffa6150f036a234fc
lyipei <lin23027@byui.edu>
2024-05-21 20:33:54 -0600

    form

--- scripts/form.js
+++ scripts/form.js
@@ -8,33 +8,14 @@ window.addEventListener('load', () => {
     modified.innerHTML = document.lastModified;
 });
 const products = [
-    {
-      id: fc-1888,
-      name: "flux capacitor",
-      averagerating: 4.5
-    },
-    {
-      id: fc-2050,
-      name: "power laces",
-      averagerating: 4.7
-    },
-    {
-      id: fs-1987,
-      name: "time circuits",
-      averagerating: 3.5
-    },
-    {
-      id: ac-2000,
-      name: "low voltage reactor",
-      averagerating: 3.9
-    },
-    {
-      id: jj-1969,
-      name: "warp equalizer",
-      averagerating: 5.0
-    }
+    { id: fc-1888, name: "flux capacitor", averagerating: 4.5 },
+    { id: fc-2050, name: "power laces", averagerating: 4.7 },
+    { id: fs-1987, name: "time circuits", averagerating: 3. },
+    { id: ac-2000, name: "low voltage reactor", averagerating: 3. },
+    { id: jj-1969, name: "warp equalizer", averagerating: 5. }
   ];
   
+document.addEventListener("DOMContentLoaded", () => {
   const productSelect = document.getElementById('productName');
 
   products.forEach(product => {
@@ -43,4 +24,4 @@ const products = [
     option.textContent = product.name;
     productSelect.appendChild(option);
   });
-  
+})
