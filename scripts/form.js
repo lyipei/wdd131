cbe21a9aa3e339e35fc7973ebd8eaf3446df01ef
lyipei <lin23027@byui.edu>
2024-05-21 21:59:57 -0600

    review

--- scripts/form.js
+++ scripts/form.js
@@ -8,20 +8,31 @@ window.addEventListener('load', () => {
     modified.innerHTML = document.lastModified;
 });
 const products = [
-    { id: fc-1888, name: "flux capacitor", averagerating: 4.5 },
-    { id: fc-2050, name: "power laces", averagerating: 4.7 },
-    { id: fs-1987, name: "time circuits", averagerating: 3. },
-    { id: ac-2000, name: "low voltage reactor", averagerating: 3. },
-    { id: jj-1969, name: "warp equalizer", averagerating: 5. }
+    { id: 'fc-1888', name: "flux capacitor", averagerating: 4.5 },
+    { id: 'fc-2050', name: "power laces", averagerating: 4.7 },
+    { id: 'fs-1987', name: "time circuits", averagerating: 3.5 },
+    { id: 'ac-2000', name: "low voltage reactor", averagerating: 3.9 },
+    { id: 'jj-1969', name: "warp equalizer", averagerating: 5.0 }
   ];
   
 document.addEventListener("DOMContentLoaded", () => {
-  const productSelect = document.getElementById('productName');
+    const productSelect = document.getElementById('productName');
 
-  products.forEach(product => {
-    const option = document.createElement('option');
-    option.value = product.id;
-    option.textContent = product.name;
-    productSelect.appendChild(option);
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.value = product.id;
+        option.textContent = product.name;
+        productSelect.appendChild(option);
   });
 })
+
+if (window.location.pathname.includes('review.html')) {
+    let reviewCount = localStorage.getItem('reviewCount');
+    if (!reviewCount) {
+    reviewCount = 0;
+    }
+    reviewCount++;
+    localStorage.setItem('reviewCount', reviewCount);
+
+    console.log('Total reviews submitted: ${reviewCount}');
+}
