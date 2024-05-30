3420146fd3ad03131a4824c936bc61f8a74a6e93
lyipei <lin23027@byui.edu>
2024-05-30 17:57:03 -0600

    media

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -319,3 +319,12 @@ document.getElementById('center').addEventListener('click', () => {
 
 // Initial display of all attractions
 displayAttractions(places);
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
